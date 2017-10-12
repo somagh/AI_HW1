3a0259f5a1ef1de822ea346a06aa1754007e5087
somagh <aryakowsary@yahoo.com>
2017-10-12 09:53:12 +0330

    beautifulsoup example

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 # in the name of god
-
-print('salam')
+import urllib.request
+from bs4 import BeautifulSoup
+parsed=BeautifulSoup(urllib.request.urlopen("http://ce.sharif.edu/~akowsary").read(),"lxml")
+print(parsed.prettify())
