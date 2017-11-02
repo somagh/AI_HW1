56119cbed379b5901af957c67eec004bd321649e
amirrezam <amirrezamozayani@gmail.com>
2017-11-02 15:16:33 +0330

    debug parser agent

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 
 import urllib.request
 from bs4 import BeautifulSoup
-# parsed = BeautifulSoup(urllib.request.urlopen("http://ce.sharif.edu/~akowsary").read(),"html.parser")
+
+parsed = BeautifulSoup(urllib.request.urlopen("http://ce.sharif.edu/~akowsary").read(),"html.parser")
 # print (parsed.body.find_all('li')[0].a.get('href'))
 # print (parsed.prettify())
 # print(list(parsed.children))
@@ -17,21 +18,36 @@ from bs4 import BeautifulSoup
 # felan = { "soup": [],"text": [] }
 
 
+a = {}
+if("salam" in a):
+    print(a["salam"])
+else:
+    a["salam"] = 1
+print(a["salam"])
+
 def parser_agent(parsed, texts, address="body"):
     parsed_first = parsed[0].select(address)[0]
     parsed_first_children = list(parsed_first.children)
     n = len(parsed_first_children)
-
+    # print(parsed_first_children, n)
+    # for i in range(n):
+    #     print(type(parsed_first_children[i]), "hi")
     parsed_per_name = {}
+
     for i in range(n):
         child_name = parsed_first_children[i].name
-        parsed_per_name[child_name] = parsed_per_name[child_name] + [parsed_first_children[i]]
+        if child_name is None:
+            continue
+        if child_name in parsed_per_name:
+            parsed_per_name[child_name] += [parsed_first_children[i]]
+        else:
+            parsed_per_name[child_name] = [parsed_first_children[i]]
+        print(i, parsed_first_children[i], child_name, parsed_per_name)
 
     max_point = 0
     max_address = ""
-
-    for key, children in parsed_per_name:
-        for i in range(len(children)):
+    for key in parsed_per_name:
+        for i in range(len(parsed_per_name[key])):
             new_address = "{} > {}:nth-of-type({})".format(address, key, i)
             point = 0
             for text in texts:
@@ -45,3 +61,5 @@ def parser_agent(parsed, texts, address="body"):
         print("answer is {}".format(address))
     else:
         parser_agent(parsed, texts, max_address)
+
+# parser_agent([parsed], ["salam"])
