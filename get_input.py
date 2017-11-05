24410a113624c136e6c6db02e6e8fdbbc61286ee
amirrezam <amirrezamozayani@gmail.com>
2017-11-05 18:13:21 +0330

    script mode

--- get_input.py
+++ get_input.py
@@ -8,12 +8,31 @@ import ssl
 
 parsed = []
 text = []
-all_data = json.load(open('irna_lead.json',encoding='utf-8'))
 
-for data in all_data:
+# all_data = json.load(open('irna_lead.json',encoding='utf-8'))
 
-    g_context = ssl._create_unverified_context()
-    parsed.append(BeautifulSoup(urllib.request.urlopen(data['link'], context=g_context).read(), "html.parser"))
-    text.append(data['text'])
+# for data in all_data:
+#
+#     g_context = ssl._create_unverified_context()
+#     parsed.append(BeautifulSoup(urllib.request.urlopen(data['link'], context=g_context).read(), "html.parser"))
+#     text.append(data['text'])
+
+inp = input()
+
+f = open(inp, 'r',encoding='utf-8')
+
+num = 0
+
+g_context = ssl._create_unverified_context()
+
+while True:
+    read = f.readline()
+    if len(read) == 0:
+        break
+    if num % 2 == 1:
+        text.append(read)
+    else:
+        parsed.append(BeautifulSoup(urllib.request.urlopen(read, context=g_context).read(), "html.parser"))
+    num += 1
 
 parser_agent(parsed, text)
